62fe54b84e421186a2488fa9fe46ace49752d5b2
Stankor <52055009+Stankor@users.noreply.github.com>
2019-06-22 21:16:25 -0400

    Update dog.js

--- commands/dog.js
+++ commands/dog.js
@@ -1,6 +1,6 @@
 // Comando de cachorro.
 const Discord = require('discord.js');
-exports.run = async (client, message, args) => {
+exports.run = (client, message, args) => {
 
     let msg = await message.channel.send('Carregando...')
 
