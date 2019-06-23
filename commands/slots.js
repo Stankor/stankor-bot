7c9ba32a4c3e2710eb6d6c438c30a3ddb7fc8f86
Stankor <52055009+Stankor@users.noreply.github.com>
2019-06-22 20:48:24 -0400

    Update slots.js

--- commands/slots.js
+++ commands/slots.js
@@ -30,3 +30,11 @@ exports.run = (client, message, args) => {
         message.channel.send(lEmbed);
     }
 } 
+
+module.exports.config = {
+    name: "slots",
+    aliases: ["cassino", "slot", "loteria"],
+    usage: ";slots",
+    description: "Cassino virtual.",
+    accessableby: "Membros"
+}
